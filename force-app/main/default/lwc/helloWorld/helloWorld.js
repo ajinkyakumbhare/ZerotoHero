bfd9c3158f374e0592c906cda5df445ab4d24c00
ajinkya <ashu.ajinkya29@gmail.com>
2023-08-08 13:44:16 +0530

    Zero to Hero

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,12 @@
+/**
+ * @description       : 
+ * @author            : PSL_Ajinkya Kumbhare-SFDX
+ * @group             : 
+ * @last modified on  : 08-08-2023
+ * @last modified by  : PSL_Ajinkya Kumbhare-SFDX
+**/
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    fullname = "Zero to Hero"
+}
